53778e4ea8e31266542dea4a93fd197aef69da99
강병규 <gangbyeong-gyu@gangbyeong-gyuui-MacBookAir.local>
2024-11-11 21:12:21 +0900

    add .

--- backend/data/code/req.py
+++ backend/data/code/req.py
@@ -4,9 +4,15 @@ import subprocess
 result = subprocess.run(['conda', 'list'], capture_output=True, text=True)
 
 # 결과 처리 및 파일에 쓰기
-with open('/Users/gangbyeong-gyu/VSCodeProjects/ML/S11P31B105/backend/requirements.txt', 'w') as f:
+output_file_path = '/Users/gangbyeong-gyu/VSCodeProjects/ML/S11P31B105/backend/requirements.txt'
+
+with open(output_file_path, 'w') as f:
     for line in result.stdout.split('\n'):
-        if line and not line.startswith('#'):
+        if line and not line.startswith('#'):  # 주석 제거
             parts = line.split()
-            if len(parts) >= 2:
-                f.write(f"{parts[0]}=={parts[1]}\n")  # = 대신 == 사용
+            if len(parts) >= 2 and not parts[0].startswith('pypi'):  # pypi 제외
+                package_name = parts[0]
+                package_version = parts[1]
+                f.write(f"{package_name}>={package_version}\n")  # '>=' 형식으로 작성
+
+print(f"Requirements file has been saved to {output_file_path}")
