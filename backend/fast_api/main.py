7e40530441c8e4595e035b6ef745d65d2eeac70b
Ubuntu <ubuntu@ip-172-26-2-62.ap-northeast-2.compute.internal>
2024-11-14 05:17:26 +0000

    add:router

--- backend/fast_api/main.py
+++ backend/fast_api/main.py
@@ -5,6 +5,7 @@ from routers.recommendation import recommendation_router
 from routers.bakeryrouter import bakeryrouter
 from routers.chat_router import chat_router
 from routers.course_router import course_router
+from routers.recommend_spot_router import recommend_router
 
 app = FastAPI(docs_url="/fast_api/docs",
               openapi_url="/fast_api/openapi.json")
@@ -22,6 +23,7 @@ app.include_router(recommendation_router, prefix="/fast_api")
 app.include_router(bakeryrouter, prefix='/fast_api')
 app.include_router(chat_router, prefix='/fast_api')
 app.include_router(course_router, prefix='/fast_api')
+app.include_router(recommend_router, prefix='/fast_api')
 
 # 기본 엔드포인트
 @app.get("/")
