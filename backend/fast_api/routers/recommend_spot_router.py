4eb5c76c526e228e77b2bd17ac661f3538bffb7c
Ubuntu <ubuntu@ip-172-26-2-62.ap-northeast-2.compute.internal>
2024-11-14 04:40:26 +0000

    add .

--- backend/fast_api/routers/recommend_spot_router.py
+++ backend/fast_api/routers/recommend_spot_router.py
@@ -8,6 +8,8 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
