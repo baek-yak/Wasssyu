f90a1918693082e52760941991d06848f668805d
happyyongku <yonggu1951@gmail.com>
2024-11-15 22:16:19 +0900

    hotfix: decode to secret key

--- backend/fast_api/jwt_handler.py
+++ backend/fast_api/jwt_handler.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import jwt
 from os import getenv
 import logging
+import base64
 
 # 로깅 설정
 logging.basicConfig(level=logging.DEBUG)
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class JWTHandler:
     def __init__(self):
-        self.secret_key = getenv("SECRET_KEY")
+        self.secret_key = base64.b64decode(getenv("SECRET_KEY"))
         self.algorithm = getenv("ALGORITHM")
         self.security = HTTPBearer()
 
